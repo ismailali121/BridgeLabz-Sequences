e8cac4eb3720071ffb585b704c12b5fe99bf57ca
ismailali121 <pathanismailalikhan@gmail.com>
2020-07-13 00:50:17 +0530

    Added program to convert 42inches to feet and calculate area of rectangular plot

--- sequences.sh
+++ sequences.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -x
-number1=${RANDOM:0:2}
-number2=${RANDOM:0:2}
-number3=${RANDOM:0:2}
-number4=${RANDOM:0:2}
-number5=${RANDOM:0:2}
-sum=$(( $number1+$number2+$number3+$number4+$number5 ))
-average=`expr $sum/5 | bc -l`
+measureInFeet=`expr 42/12 | bc -l`
+rectangleLength=60
+rectangleWidth=40
+plotAreaInFeet=$(( 60*40 ))
+plotAreaInMeters=`expr $plotAreaInFeet\*0.3048 | bc -l`
+plotAreaInAcers=`expr $plotAreaInMeters\*0.000247 | bc -l`
+TwentyFiveplotsAreaInAcers=`expr 25\*$plotAreaInAcers | bc -l`
+
