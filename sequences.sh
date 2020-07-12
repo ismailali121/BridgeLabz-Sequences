491133cf1edb8247b08342a860d492b79920f949
ismailali121 <pathanismailalikhan@gmail.com>
2020-07-13 00:44:19 +0530

    Added program to reads 5 random 2 digit values and find sum and average

--- sequences.sh
+++ sequences.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
-number1=$((RANDOM%6+1))
-echo "The first random number is : $number1"
-number2=$((RANDOM%6+1))
-echo "The second random number is : $number2"
-sum=$(( $number1 + $number2 ))
+number1=${RANDOM:0:2}
+number2=${RANDOM:0:2}
+number3=${RANDOM:0:2}
+number4=${RANDOM:0:2}
+number5=${RANDOM:0:2}
+sum=$(( $number1+$number2+$number3+$number4+$number5 ))
+average=`expr $sum/5 | bc -l`
